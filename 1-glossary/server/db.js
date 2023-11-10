1bb5d6ed3f1251b30f30d664ac5ba94fdd5b0012
Justin Cordova <justcord10@gmail.com>
2023-11-10 13:15:53 -0800

    server and database fully implemented and tested

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,24 @@
+// 0. Import Required Dependencies (dotenv and mongoose)
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//test connectivity
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongo database');
+});
+
 // 2. Set up any schema and models needed by the app
+const glossaryEntrySchema = new mongoose.Schema({
+  keyword: String,
+  definition: String
+});
+
+const GlossaryEntry = mongoose.model('GlossaryEntry', glossaryEntrySchema);
 // 3. Export the models
+module.exports = GlossaryEntry;
 // 4. Import the models into any modules that need them
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,29 @@
+// Dependencies Here ////////////////////////////////////////////////////
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
+const controllers = require('./controllers.js')
+const morgan = require('morgan');
+// Creste Express Server ////////////////////////////////////////////////
 const app = express();
 
-// Serves up all static and generated assets in in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+// Middleware Here //////////////////////////////////////////////////////
 
-/****
- *
- *
- * Other routes here....
- *
- *
- */
+app.use(express.json());
+// Serves up all static and generated assets in in a specified folder.
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(morgan('dev'));
+// Routes Here //////////////////////////////////////////////////////////
+app.get('/glossary', controllers.getGlossary);
+
+app.post('/glossary', controllers.addEntry);
+
+app.patch('/glossary/:id', controllers.updateEntry);
+
+app.delete('/glossary/:id', controllers.deleteEntry);
+
+
+// Port Def /////////////////////////////////////////////////////////////
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
