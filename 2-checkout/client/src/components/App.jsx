1185c227da3a4d9160adbade6a2e75608b24133c
Justin Cordova <justcord10@gmail.com>
2023-11-11 16:43:41 -0800

    confirmed that the page renders the skeleton properly, will implement conditional rendering AFTER states, handlers, and axios API calls are implemented.

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Axios from 'axios';
 import Cart from './Cart.jsx';
 import Form1 from './Form1.jsx';
 import Form2 from './Form2.jsx';
@@ -6,10 +7,17 @@ import Form3 from './Form3.jsx';
 import Confirmation from './Confirmation.jsx';
 
 
-const App = () => {
-
+const App = ({cookie}) => {
+  console.log(cookie);
   return (
-    <div>App</div>
+    <div>
+      <div>App</div>
+      <div><Cart /></div>
+      <div><Form1 /></div>
+      <div><Form2 /></div>
+      <div><Form3 /></div>
+      <div><Confirmation /></div>
+    </div>
   );
 };
 
